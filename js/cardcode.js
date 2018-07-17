ed750b38553185535ed642bf0ad01dce0368cbd1
Gopinath Rajamani <goprajam@publicisgroupe.net>
2018-07-17 17:43:19 +0530

    changes updated

--- js/cardcode.js
+++ js/cardcode.js
@@ -1,22 +1,52 @@
+/*Ajax calls */
+let Popularlist,Popularlistdat,SearchList,SearchData,CollectionHtml,PopularHtml;
+const Apikey="d785263959aca38cc69b73f271297635";
+const Baseurl="https://api.themoviedb.org/3/";
+
+function ajaxcall(ApiUrl,Apikey){
+
+    var data = $.parseJSON($.ajax({
+            url: ApiUrl + "api_key="+ Apikey +"&language=en-US",
+            dataType: 'json', 
+            async: false
+        }).responseText);
+     return data["results"]   ;
+}
+
+function poplistgen(movielg,classname,datagen){
+    PopularHtml=""
+    for(i=0;i<movielg;i++){
+        PopularHtml +="<div class='card p-4 my-flex-item'><div class='imgcls text-center'><img class='card-img-top' src='https://image.tmdb.org/t/p/w500/"+datagen[i].poster_path +"' alt='Card image cap' /><div class='card-text'><span><i class='fa fa-heart'></i></span>"+ datagen[i].title +"</div></div></div>";
+    }
+    document.getElementsByClassName(classname)[0].innerHTML = PopularHtml;
+
+}
+function poplistall(arrdata) {
+    $("#DialogOpen").trigger("click")
+    poplistgen(arrdata.length,"popbody",arrdata)
+      
+};
+
+
+
 
 /* Popular list */
-var card="";
-for(i=0;i<5;i++){
- card +="<div class='card col-12 col-md-2 ml-2'><div class='imgcls text-center'><img class='card-img-top' src='./assets/movieone.jpg' alt='Card image cap'></div><div class=card-body>  <p class='card-text'>Lorem ipsum dolor sit amet, consectetur</p></div></div>";
-}
-
-
-document.getElementsByClassName("cardcust")[0].innerHTML += card;
-
+    Popularlist=ajaxcall("https://api.themoviedb.org/3/movie/popular?",Apikey);
+    poplistgen(5,"cardcust",Popularlist);
 
 /* Collection list */
-var collection="";
-for(i=0;i<4;i++){
-    collection +="<div class='card col-12 col-md-2 ml-2'><div class='imgcls text-center'><img class='card-img-top' src='./assets/movieone.jpg' alt='Card image cap'></div><div class='card-body'>  <p class='card-text'>Lorem ipsum dolor sit amet, consectetur</p></div></div>";
-}
-collection +="<div class='col-md-12'><button class='float-right'>Show More</button></div>"
+    for(i=0;i<4;i++){
+        CollectionHtml +="<div class='card col-12 col-md-2 ml-2'><div class='imgcls text-center'><img class='card-img-top' src='./assets/movieone.jpg' alt='Card image cap'></div><div class='card-body'>  <p class='card-text'>Lorem ipsum dolor sit amet, consectetur</p></div></div>";
+    }
+    CollectionHtml +="<div class='col-md-12'><button class='float-right'>Show More</button></div>"
+    document.getElementsByClassName("collection")[0].innerHTML += CollectionHtml;
 
-document.getElementsByClassName("collection")[0].innerHTML += collection;
+    document.getElementById("PopListId").addEventListener("click", poplistall.bind(this,Popularlist));
+    //document.getElementById("CollViewAll").addEventListener("click", CollListAll.bind(this,CollectionList));
+  
+ 
+/* search results */
+    //SearchList=ajaxcall("https://api.themoviedb.org/3/search/movie?query="+SearchData+"&page=1&include_adult=false",Apikey);
 
 
 
--- js/index.js
+++ js/index.js
@@ -1,3 +1,7 @@
+
+require ("../node_modules/jquery/dist/jquery.min.js");
+require ("../node_modules/popper.js/dist/popper.min.js");
+require ("../node_modules/bootstrap/dist/js/bootstrap.min.js");
 require ("./cardcode.js");
 require ("../scss/style.scss");
 
