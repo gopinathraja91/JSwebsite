a11636a3a0ba8c940834f79f20961b64d69b949b
Gopinath Rajamani <goprajam@publicisgroupe.net>
2018-07-18 17:17:31 +0530

    changes

--- js/cardcode.js
+++ js/cardcode.js
@@ -1,52 +1,137 @@
 /*Ajax calls */
-let Popularlist,Popularlistdat,SearchList,SearchData,CollectionHtml,PopularHtml;
+let Popularlist,Popularlistdat,SearchData,CollectionHtml,PopularHtml,PopAllTimedata;
 const Apikey="d785263959aca38cc69b73f271297635";
 const Baseurl="https://api.themoviedb.org/3/";
+const BaseServerUrl="http://localhost:3000/products";
 
-function ajaxcall(ApiUrl,Apikey){
+function ajaxcall(ApiUrl,Apikey,DataType){
 
     var data = $.parseJSON($.ajax({
             url: ApiUrl + "api_key="+ Apikey +"&language=en-US",
             dataType: 'json', 
             async: false
         }).responseText);
-     return data["results"]   ;
-}
-
-function poplistgen(movielg,classname,datagen){
-    PopularHtml=""
-    for(i=0;i<movielg;i++){
-        PopularHtml +="<div class='card p-4 my-flex-item'><div class='imgcls text-center'><img class='card-img-top' src='https://image.tmdb.org/t/p/w500/"+datagen[i].poster_path +"' alt='Card image cap' /><div class='card-text'><span><i class='fa fa-heart'></i></span>"+ datagen[i].title +"</div></div></div>";
+    
+    if(DataType=="Json"){ 
+        return data["results"];
+    }else{
+        return data;
     }
-    document.getElementsByClassName(classname)[0].innerHTML = PopularHtml;
+     
+}
+
+
+function AjaxPost(URL,data){
+    for(i=0;i<data.length;i++){
+        $.post("http://localhost:3000/products",
+        data[i],
+        function(data, status){
+            alert("Data: " + data + "\nStatus: " + status);
+        });
+    }
 
 }
-function poplistall(arrdata) {
-    $("#DialogOpen").trigger("click")
-    poplistgen(arrdata.length,"popbody",arrdata)
-      
+
+function AjaxDelete(URL,data){
+    for(i=0;i<data.length;i++){
+        console.log(data[i].id);
+            $.ajax({
+            url: "http://localhost:3000/products/"+data[i].id,
+            type: 'DELETE',
+            success: function(response) {
+             
+            }
+         });
+    }
+
+}
+
+
+
+
+function poplistgen(movielg,classname,datagen,delbtnsta,headcont){
+              
+        PopularHtml="";
+        for(i=0;i<movielg;i++){
+            PopularHtml +=`<div class='card p-2 ml-2 my-flex-item'><div class='round'><input type='checkbox' id='${classname + i}' name='${datagen[i].id}' /><label for='${classname + i}' ></label></div><div class='imgcls text-center'><img class='card-img-top' src='https://image.tmdb.org/t/p/w500/${datagen[i].poster_path}' alt='Card image cap' /><div class='card-text'><span><i class='fa fa-heart'></i></span><span>&nbsp${datagen[i].vote_average * 10}% &nbsp&nbsp</span><span>${datagen[i].title}</span></div></div></div>`;
+        }
+        document.getElementsByClassName(classname)[0].innerHTML = PopularHtml;        
+        PopAllTimedata=datagen;
+        if(delbtnsta=="delete"){
+            $("#DelCollec").css("display","block");
+            $("#AddCollec").css("display","none");
+            
+        }else{
+            $("#DelCollec").css("display","none"); 
+            $("#AddCollec").css("display","block");           
+        }
+        $(".modal-title").html(headcont)
+}
+
+
+function poplistall(arrdata,delbtnstatus,headcont) {
+
+        $("#DialogOpen").trigger("click")
+        poplistgen(arrdata.length,"popbody",arrdata,delbtnstatus,headcont)
+        
 };
+
+function SearchListFun() {
+
+        let searcval=$("[aria-label='Search']").val()
+        SearchListdata=ajaxcall("https://api.themoviedb.org/3/search/movie?query="+searcval+"&page=1&include_adult=false&",Apikey,"Json");
+        $("#DialogOpen").trigger("click")
+        poplistgen(SearchListdata.length,"popbody",SearchListdata,"NOdelete","Search Results")
+
+};
+
+function Addcollection(){
+       
+        let selected = [];
+        $('.popbody input:checked').each(function() {
+            selected.push($(this).attr('name'));
+        });       
+        let filterObj = PopAllTimedata.filter(function(e) {
+                return selected.includes(e.id.toString());
+        });       
+        
+        AjaxPost(BaseServerUrl,filterObj);        
+}
+
+function DelCollecList(){
+
+    let selected = [];
+    $('.popbody input:checked').each(function() {
+        selected.push($(this).attr('name'));
+    });   
+    let filterObj = PopAllTimedata.filter(function(e) {
+            return selected.includes(e.id.toString());
+    }); 
+
+    AjaxDelete(BaseServerUrl,filterObj);
+}
 
 
 
 
 /* Popular list */
-    Popularlist=ajaxcall("https://api.themoviedb.org/3/movie/popular?",Apikey);
-    poplistgen(5,"cardcust",Popularlist);
+
+    Popularlist=ajaxcall("https://api.themoviedb.org/3/movie/popular?",Apikey,"Json");
+    poplistgen(5,"cardcust",Popularlist,"NOdelete","");
 
 /* Collection list */
-    for(i=0;i<4;i++){
-        CollectionHtml +="<div class='card col-12 col-md-2 ml-2'><div class='imgcls text-center'><img class='card-img-top' src='./assets/movieone.jpg' alt='Card image cap'></div><div class='card-body'>  <p class='card-text'>Lorem ipsum dolor sit amet, consectetur</p></div></div>";
-    }
-    CollectionHtml +="<div class='col-md-12'><button class='float-right'>Show More</button></div>"
-    document.getElementsByClassName("collection")[0].innerHTML += CollectionHtml;
+    CollectionList=ajaxcall("http://localhost:3000/products?",Apikey,"Array")
+    colleng=(CollectionList.length>4)?4:CollectionList.length;
+    poplistgen(CollectionList.length,"collection",CollectionList,"NOdelete","");
 
-    document.getElementById("PopListId").addEventListener("click", poplistall.bind(this,Popularlist));
-    //document.getElementById("CollViewAll").addEventListener("click", CollListAll.bind(this,CollectionList));
-  
+/*Button Click Event Listeners */
+    document.getElementById("PopListId").addEventListener("click", poplistall.bind(this,Popularlist,"NOdelete","Popular Lists"));    
+    document.getElementById("searchbtn").addEventListener("click", SearchListFun);
+    document.getElementById("AddCollec").addEventListener("click", Addcollection);
+    document.getElementById("ViewCollec").addEventListener("click", poplistall.bind(this,CollectionList,"delete","Collection Lists"));
+    document.getElementById("DelCollec").addEventListener("click", DelCollecList);
+    
  
-/* search results */
-    //SearchList=ajaxcall("https://api.themoviedb.org/3/search/movie?query="+SearchData+"&page=1&include_adult=false",Apikey);
 
 
 
