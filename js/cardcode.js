19fe443704ec10a45bf22c61d2fd21dce207450a
Gopinath Rajamani <goprajam@publicisgroupe.net>
2018-07-19 12:31:22 +0530

    changes today

--- js/cardcode.js
+++ js/cardcode.js
@@ -53,7 +53,7 @@ function poplistgen(movielg,classname,datagen,delbtnsta,headcont){
               
         PopularHtml="";
         for(i=0;i<movielg;i++){
-            PopularHtml +=`<div class='card p-2 ml-2 my-flex-item'><div class='round'><input type='checkbox' id='${classname + i}' name='${datagen[i].id}' /><label for='${classname + i}' ></label></div><div class='imgcls text-center'><img class='card-img-top' src='https://image.tmdb.org/t/p/w500/${datagen[i].poster_path}' alt='Card image cap' /><div class='card-text'><span><i class='fa fa-heart'></i></span><span>&nbsp${datagen[i].vote_average * 10}% &nbsp&nbsp</span><span>${datagen[i].title}</span></div></div></div>`;
+            PopularHtml +=`<div class='card p-2 ml-2 my-flex-item'><div class='round'><input type='checkbox' id='${classname + i}' name='${datagen[i].id}' /><label for='${classname + i}' ></label></div><div class='imgcls text-center' id='${datagen[i].id}' ><img class='card-img-top' src='https://image.tmdb.org/t/p/w500/${datagen[i].poster_path}' alt='Card image cap' /><div class='card-text'><span><i class='fa fa-heart'></i></span><span>&nbsp${datagen[i].vote_average * 10}% &nbsp&nbsp</span><span>${datagen[i].title}</span></div></div></div>`;
         }
         document.getElementsByClassName(classname)[0].innerHTML = PopularHtml;        
         PopAllTimedata=datagen;
@@ -130,6 +130,30 @@ function DelCollecList(){
     document.getElementById("AddCollec").addEventListener("click", Addcollection);
     document.getElementById("ViewCollec").addEventListener("click", poplistall.bind(this,CollectionList,"delete","Collection Lists"));
     document.getElementById("DelCollec").addEventListener("click", DelCollecList);
+
+
+    $(".imgcls").click(function(){
+
+
+        var selector=$(this).attr("id");
+        $("#DialogOpen").trigger("click")
+        PopularHtml="";
+        let filterObj = Popularlist.filter(function(e) {
+            return selector.toString() == e.id.toString();
+         }); 
+
+         if(filterObj.length==0){
+               filterObj = CollectionList.filter(function(e) {
+                     return selector.toString() == e.id.toString();
+                }); 
+         }
+        PopularHtml +=`<div class='card p-2 ml-2 my-flex-item'><div class="row"><div class="col-md-6 float-left"><div class='imgcls text-center' id='${filterObj[0].id}' ><img class='card-img-top' src='https://image.tmdb.org/t/p/w500/${filterObj[0].poster_path}' alt='Card image cap' /><p><span class="r-corner">2D</span><span class="r-corner">3D</span></p></div></div><div class="col-md-6"><h3>Movie Overview</h3><div class='card-text'><span><i class='fa fa-heart'></i></span><span>&nbsp${filterObj[0].vote_average * 10}% &nbsp&nbsp</span><span>Votes: ${filterObj[0].vote_count}</span><span>&nbsp 4.3<i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i></span></div><p>${filterObj[0].overview}</p><p>Release Date: ${filterObj[0].release_date}</p></div></div>`;
+        document.getElementsByClassName("popbody")[0].innerHTML = PopularHtml; 
+        $(".modal-title").html("Movie Info") ; 
+        $("#DelCollec").css("display","none");  
+        $(".modal-footer").html("<button type='button' class='btn btn-danger' data-dismiss='modal'>Book now</button><button type='button' class='btn btn-danger' data-dismiss='modal'>Close</button>") ;  
+
+    })
     
  
 
