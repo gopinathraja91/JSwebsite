4a4f33d7710eb4c41f6bd3b22783f72a7f88a989
Gopinath Rajamani <goprajam@publicisgroupe.net>
2018-07-24 17:34:15 +0530

    changes done

--- js/cardcode.js
+++ js/cardcode.js
@@ -1,11 +1,42 @@
 const _var = require('./variables.js');
 import { PopularList_Render,CollectionList_Render,Button_events,ImageInfo,Popuphtml } from "../render/renderpage.js"
+import {createStore} from 'redux';
 
-PopularList_Render();
-CollectionList_Render();
-Popuphtml();
-ImageInfo();
-Button_events();
+// Reducer
+function CreateComponent(state={items:[]}, action) {
+    switch(action.type) {
+      case 'Create Components':
+        return {items: "Inital Load"};
+        break;
+      default:
+        return state;
+    }
+  }
+
+  
+// Instantiating the store
+const store = createStore(CreateComponent, {
+    items:[
+      "Load Popular List",
+      "Load Collection List"
+    ]
+  });
+
+  
+// Listen for changes
+store.subscribe(() => {
+    PopularList_Render();    
+    CollectionList_Render();
+    Popuphtml();
+    ImageInfo();
+    Button_events();
+});
+
+// Trigger Events
+store.dispatch({type: 'Create Components', item: "Initial Load"});
+
+
+
 
     
     
